bfd279ca5e9cc0c28b662c1cb9a0833dce73dfe9
Stanley Lee <stanigator@gmail.com>
2012-05-19 15:20:25 -0700

    deleted all the files

--- first_app/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-RailsSample::Application.config.secret_token = '680460f1f85ee133be6ac5ba6f8995d6e0bc6fc0bcc8fdb517b0b2d2f44ff45a827f35c9dc6d2d31d1bd78563a284d0b83105a7a117e9ff58015498e1e714cc0'
